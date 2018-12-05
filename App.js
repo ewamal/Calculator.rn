0d37147b1559bb489b1bad8204ec07894ac2fc0f
ewamal <maluseckae@gmail.com>
2018-12-05 18:20:55 +0100

    add dropdown :lipstick:

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Picker } from 'react-native';
+
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { text: 'Add an array...'}
+    this.handleChangeText = this.handleChangeText.bind(this)
+  }
+
+  handleChangeText(text){
+    this.setState({ text: text })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}>Calculator</Text>
+
+        <TextInput
+          onChangeText={this.handleChangeText}
+          value={this.state.text}
+        />
+
+        <Picker
+          selectedValue={this.state.type}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) => this.setState({type: itemValue})}>
+          <Picker.Item label="mode" value="mode" />
+          <Picker.Item label="median" value="median" />
+          <Picker.Item label="average" value="average" />
+          <Picker.Item label="add" value="add" />
+        </Picker>
       </View>
     );
   }
@@ -14,8 +40,12 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white',
+    fontSize: 50
+  }
 });
