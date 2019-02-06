3cb8f641f9e4e93f86f638474f7ccef5f61bc989
ewamal <maluseckae@gmail.com>
2019-02-06 15:00:45 +0100

    add numbers dont have to be in array :zap:

--- App.js
+++ App.js
@@ -1,22 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Picker } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Picker, Button } from 'react-native';
 
 
 export default class App extends React.Component {
+
   constructor(props){
     super(props);
-    this.state = { text: 'Add an array...'}
+    this.state = {
+      text: '',
+      type: "mode",
+      result: null,
+      errorMessage: null,
+      numbers: []
+    }
     this.handleChangeText = this.handleChangeText.bind(this)
+    this.handlePress = this.handlePress.bind(this)
+    this.handleAddPress = this.handleAddPress.bind(this)
   }
 
   handleChangeText(text){
     this.setState({ text: text })
+
   }
 
-  onFocus() {
-    this.setState({
-      text: ''
+  handleAddPress(){
+    const {numbers, text} = this.state;
+    this.setState({numbers: [...numbers, text]}, () => {
+      console.log(this.state.numbers);
     })
+    
+  }
+
+  handlePress(){
+    let array = this.state.numbers;
+    array = array.map(num => parseInt(num));
+    
+    // try {
+    //   array = JSON.parse(this.state.numbers);
+    // } catch(e) {
+    //   console.warn(e)
+    // }
+
+    const baseUrl = 'https://calculator-ewa.herokuapp.com'
+    const data = {type: this.state.type, array: array}
+
+    fetch(`${baseUrl}/evaluate`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => this.setState({ result: data[this.state.type], errorMessage: data.error_message }));
   }
 
   render() {
@@ -25,20 +61,37 @@ export default class App extends React.Component {
         <Text style={styles.text}>Calculator</Text>
 
         <TextInput
+          placeholder={'add an number'}
+          style={styles.input}
           onChangeText={this.handleChangeText}
           value={this.state.text}
-          onFocus={() => this.onFocus()}
         />
+
+        <Button
+        onPress={this.handleAddPress}
+        title='🔥' 
+        />
+        <Text>
+          {this.state.numbers.join(' , ')}
+        </Text>
 
         <Picker
           selectedValue={this.state.type}
-          style={{ height: 50, width: 100 }}
+          style={styles.picker}
           onValueChange={(itemValue, itemIndex) => this.setState({type: itemValue})}>
           <Picker.Item label="mode" value="mode" />
           <Picker.Item label="median" value="median" />
           <Picker.Item label="average" value="average" />
           <Picker.Item label="add" value="add" />
         </Picker>
+
+        <Button
+        onPress={this.handlePress}
+        title='calculate'
+        />
+
+        <Text>result {this.state.result}</Text>
+        <Text>{this.state.errorMessage}</Text>
       </View>
     );
   }
@@ -48,11 +101,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'pink',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
   text: {
     color: 'white',
     fontSize: 50
+  },
+  input: {
+    fontSize: 30,
+    color: '#aa7e8e'
+  },
+  picker: {
+    width: 100
   }
 });
